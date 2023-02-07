1ad68db3315c5121c99ad7e7ccddf23400c7eacd
sonhyeongseok <mcpe4656@naver.com>
2023-02-07 19:30:58 +0900

    Add Extent + Add PartialStream

---
+++ Util.IO/Extent.cs
@@ -0,0 +1,14 @@
+﻿namespace Util.IO
+{
+    internal class Extent
+    {
+        public long Start { get; set; }
+        public long Size { get; set; }
+
+        public Extent(long start, long size)
+        {
+            Start = start;
+            Size = size;
+        }
+    }
+}
