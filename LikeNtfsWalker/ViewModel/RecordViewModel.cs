20932f8c156db43a4f4f02c441a4e39dea9d36e6
jjangdol02 <97338594+jjangdol02@users.noreply.github.com>
2023-02-11 11:57:23 +0900

    Add UI Second

--- LikeNtfsWalker/ViewModel/RecordViewModel.cs
+++ LikeNtfsWalker/ViewModel/RecordViewModel.cs
@@ -1,7 +1,47 @@
-﻿namespace LikeNtfsWalker.ViewModel
+﻿using LikeNtfsWalker.Model;
+using LikeNtfsWalker.UI;
+using System.Collections.ObjectModel;
+
+namespace LikeNtfsWalker.ViewModel
 {
-    public class RecordViewModel
+    public class RecordViewModel : Notifier
     {
-        
+        private ObservableCollection<Record> recordslist;
+
+        public ObservableCollection<Record> REcordList
+        {
+            get => recordslist;
+            set
+            {
+                recordslist = value;
+                RaisePropertyChanged();
+            }
+        }
+
+        private ObservableCollection<Record> fileinfolist;
+
+        public ObservableCollection<Record> Fileinfolist
+        {
+            get => fileinfolist;
+            set
+            {
+                fileinfolist = value;
+                RaisePropertyChanged();
+            }
+        }
+
+        public Command SaveCommand { get; set; }
+
+        public RecordViewModel()
+        {
+            recordslist = new ObservableCollection<Record>();
+            fileinfolist = new ObservableCollection<Record>();
+            SaveCommand = new Command(Savefile);
+        }
+
+        public void Savefile(object parameter)
+        {
+            // 스캔한 정보 파일로 저장
+
+        }
     }
-}
